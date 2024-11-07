0d6f75740adc7430ee53cd61cc75a00f55827a8f
Ardiyan Mahessa <ardxyns@gmail.com>
2024-11-07 22:33:31 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// set true if want to use proxy, false if not
+// ganti true jika ingin menggunakan proxy, false jika tidak
+// установите true, если хотите использовать прокси, false, если нет
+const useProxy = false; 
+
+
+
+module.exports = {
+
+  useProxy
+
+};
