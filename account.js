287bbc4b123ece489c573072182683d82eebe10a
Ardiyan Mahessa <ardxyns@gmail.com>
2024-11-08 09:04:20 +0700

    Update account.js

--- account.js
+++ account.js
@@ -1,1 +1,13 @@
-
+module.exports = [
+  {
+    email: "account1@example.com",
+    password: "password1"
+  },
+  {
+    email: "account2@example.com",
+    password: "password2"
+  },
+  // Add More Accounts As Needed
+  // Tambahin Akun Sesuai Kebutuhan Mu
+  // Добавляйте больше учетных записей по мере необходимости
+];
